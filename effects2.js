7695e4f25195b11704d8b4bd9f63f9fa4e0b3995
tanjiro_habshi <97285415+tanjiro-habshi@users.noreply.github.com>
2024-12-08 05:26:11 +0000

    yes

--- effects2.js
+++ effects2.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const imgSrc = "https://lanyard.cnrad.dev/api/1298705688067051572?hideSpotify=false&hideActivity=false&hideBadges=false&hideTimestamp=false&hideDecoration=false&bg=00000&borderRadius=10px&idleMessage=Hi&theme=dark&hideStatus=false";
+    const imgSrc = "https://lanyard.cnrad.dev/api/1279606951113658450?hideSpotify=false&hideActivity=false&hideBadges=false&hideTimestamp=false&hideDecoration=false&bg=00000&borderRadius=10px&idleMessage=Hi&theme=dark&hideStatus=false";
     const imgElement = document.getElementById('rpc');
     imgElement.src = imgSrc;
 });
